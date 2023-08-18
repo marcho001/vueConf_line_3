781ec1b14d569414e1785685de056dc5297c205d
william1123 <william.pan@accuhit.net>
2023-08-18 15:55:01 +0800

    add copybutton.vue

---
+++ src/components/CopyButton.vue
@@ -0,0 +1,29 @@
+<script setup>
+const props = defineProps({
+  label: {
+    type: String,
+    default: '複製按鈕',
+  },
+  copyText: {
+    type: String,
+    default: '複製的文字',
+  },
+})
+const handleClick = async () => {
+  await navigator.clipboard.writeText(props.copyText)
+  alert(`已複製{${props.copyText}}`)
+}
+</script>
+
+<template>
+  <button :class="$attrs.class" type="button" @click="handleClick">
+    {{ label }}
+  </button>
+</template>
+
+<style scoped>
+button {
+  width: 200px;
+  height: 50px;
+}
+</style>
