c53423751e303863afb900de51bc01ad30712583
x94jp6rmp <x94jp6rmp@gmail.com>
2023-08-18 16:18:17 +0800

    btn component

---
+++ src/components/BtnCustom.vue
@@ -0,0 +1,72 @@
+<template>
+	<button :class="type" :disabled="disabled" @click="callback($event)">
+		<slot></slot>
+	</button>
+</template>
+
+<script>
+export default {
+	name: "BtnCustom",
+	props: {
+		type: { default: "normal" },
+		disabled: Boolean,
+	},
+	data() {
+		return {};
+	},
+	methods: {
+		callback: function (e) {
+			this.$emit("click", e);
+		},
+	},
+};
+</script>
+<style>
+button {
+	padding: 10px 20px;
+	border: 1px solid #ddd;
+	color: #333;
+	background-color: #fff;
+	border-radius: 5px;
+	font-size: 1rem;
+	cursor: pointer;
+	margin-right: 10px;
+	&[disabled] {
+		cursor: not-allowed;
+		opacity: 0.7;
+	}
+	&.danger {
+		background-color: #e24d4d;
+		border: none !important;
+		color: #fff;
+	}
+	&.danger:hover {
+		box-shadow: 6px 6px #ff8585;
+		transition: 0.3s;
+		background-color: #c84141;
+	}
+	&.success {
+		background-color: #26c76f;
+		color: #fff;
+	}
+	&.success:hover {
+		box-shadow: 6px 6px #57f39d;
+		transition: 0.3s;
+		background-color: #23a55d;
+	}
+	&.info {
+		background-color: #50bfff;
+		color: #fff;
+	}
+	&.info:hover {
+		box-shadow: 6px 6px #a0dcff;
+		transition: 0.3s;
+		background-color: #419fd5;
+	}
+	&.normal:hover {
+		box-shadow: 6px 6px #bbbbbb;
+		transition: 0.3s;
+		background-color: #e8e8e8;
+	}
+}
+</style>
--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 export { default as HelloWord } from "./components/HelloWorld.vue";
+export { default as BtnCustom } from "./components/BtnCustom.vue";
